753aea1b7f518980b37f08831abe9472f347492a
Wei Lu <55832638+Rainiereed@users.noreply.github.com>
2021-05-03 15:59:53 +0200

    Add files via upload

---
+++ example.js
@@ -0,0 +1,7 @@
+import { motion } from "framer-motion";
+
+import React from "react";
+
+export default function example() {
+  return <motion.div animate={{ scale: 1.1 }}>hello motion</motion.div>;
+}
